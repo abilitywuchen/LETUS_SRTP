6c02635e8dd73cfb4ef511eb8081d39919c2d87a
Lin Runduo <2444088970@qq.com>
2025-07-29 22:49:37 +0800

    pool with allocate function

--- lib/ElementPool.hpp
+++ lib/ElementPool.hpp
@@ -2,6 +2,7 @@
 
 #include <cstdlib>
 #include <cstring>
+#include <vector>
 
 template <typename T>
 class ElementPool {
@@ -18,9 +19,17 @@ class ElementPool {
         }
     }
     inline T* allocate() {
+        if (!free_list.empty()) {
+            T* obj = free_list.back();
+            free_list.pop_back();
+            return obj;
+        }
         if (size_ >= capacity_)
             reserve();
         return &pool_[size_++];
+    }
+    void deallocate(T* obj) {
+        free_list.push_back(obj);
     }
     void reserve() {
         // std::cout << "Reserving " << capacity_ << std::endl;
@@ -42,6 +51,7 @@ class ElementPool {
     size_t capacity_ = 0;
     size_t size_ = 0;
     std::vector<T*> garbage;
+    std::vector<T*> free_list; // 只记录被释放的块 首选查找被释放的
 
 };
 
@@ -59,9 +69,17 @@ class PagePool {
         }
     }
     inline char* allocate() {
+        if (!free_list.empty()) {
+            char* page = free_list.back();
+            free_list.pop_back();
+            return page;
+        }
         if (size_ >= capacity_)
             reserve();
         return &pool_[(size_++) * PAGE_SIZE];
+    }
+    void deallocate(char* page) {
+        free_list.push_back(page);
     }
     void reserve() {
         // std::cout << "Reserving " << capacity_ << std::endl;
@@ -84,5 +102,6 @@ class PagePool {
     size_t capacity_ = 0;
     size_t size_ = 0;
     std::vector<char*> garbage;
+    std::vector<char*> free_list;
 
 };
